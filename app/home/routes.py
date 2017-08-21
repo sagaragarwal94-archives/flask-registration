2e023a4fdd519d98db23d85da3c9b34ef03cf411
sagaragarwal94 <atomsagar@gmail.com>
2017-08-21 10:49:58 +0100

    [#3] Templates File Loaded
    Signed-off-by: Sagar Chand Agarwal <sagarchand.agarwal@inspirelabs.in>

--- app/home/routes.py
+++ app/home/routes.py
@@ -5,3 +5,13 @@ from app.home import home
 @home.route('/')
 def start():
     return render_template('index.html')
+
+
+@home.route('/sign')
+def sign():
+    return render_template('home/sign.html')
+
+
+@home.route('/register')
+def register():
+    return render_template('home/register.html')
